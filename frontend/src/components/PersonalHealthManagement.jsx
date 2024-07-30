421f52fe5b77b5f806973f6f2953ebd16529c032
Swastika7883 <swastika2004@gmail.com>
2024-07-30 16:11:30 +0530

    new pages

--- frontend/src/components/PersonalHealthManagement.jsx
+++ frontend/src/components/PersonalHealthManagement.jsx
@@ -10,7 +10,12 @@ const PersonalHealthManagement = () => {
   const services = [
     { title: 'Initial Assessment', duration: '1 hr', price: '$75', imgSrc: 'https://plus.unsplash.com/premium_photo-1661777196224-bfda51e61cfd?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8eW9nYXxlbnwwfHwwfHx8MA%3D%3D' },
     { title: 'Personal Counseling', duration: '1 hr', price: '$75', imgSrc: 'https://plus.unsplash.com/premium_photo-1676408941292-c34f05151747?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
+    { title: 'Lifestyle Workshop', duration: '1 hr', price: '$75', imgSrc: 'https://blog.myfitnesspal.com/wp-content/uploads/2022/05/Eating-For-Impact-6-Ways-to-Eat-for-Focus-2.jpg.webp' },
+    { title: 'Lifestyle Workshop', duration: '1 hr', price: '$75', imgSrc: 'https://blog.myfitnesspal.com/wp-content/uploads/2018/01/UACF_EG_Hero_NoBadge_Portion-Control.jpg.webp' },
+    { title: 'Lifestyle Workshop', duration: '1 hr', price: '$75', imgSrc: 'https://blog.myfitnesspal.com/wp-content/uploads/2022/02/5-Things-I-Learned-Working-With-Elite-Athletes.jpg.webp' },
+    { title: 'Lifestyle Workshop', duration: '1 hr', price: '$75', imgSrc: 'https://blog.myfitnesspal.com/wp-content/uploads/2024/05/gut-health-weight-loss.jpg' },
     { title: 'Lifestyle Workshop', duration: '1 hr', price: '$75', imgSrc: 'https://images.unsplash.com/photo-1430163393927-3dab9af7ea38?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
+    { title: 'Lifestyle Workshop', duration: '1 hr', price: '$75', imgSrc: 'https://static.wixstatic.com/media/2a1a02_45bbbb204c1c42d5b2997c88312047f9~mv2.jpeg/v1/fill/w_400,h_300,fp_0.50_0.50,q_90,enc_auto/2a1a02_45bbbb204c1c42d5b2997c88312047f9~mv2.jpeg' },
   ];
 
   const handleBookNow = (service) => {
