68efefd686e1d6a49c92b2daef31b547f0313947
Aayush Kukreja <aayushkukreja0104@gmail.com>
2024-08-03 07:19:45 +0530

    temp

--- GreenPath/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset(); // Unset all session variables
-session_destroy(); // Destroy the session
-
-header("Location: index.php"); // Redirect to home page
-exit();
-?>
